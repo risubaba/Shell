503895a985bc3972a80ea89d403c6c80a52c3357
risubaba <risubhjain1010@gmail.com>
2019-08-19 00:27:53 +0530

    ls done except -la

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
+#include <dirent.h>
+
 #define INP_MAX 100
 
 char cwd[PATH_MAX];
 char swd[PATH_MAX];
 char ret[PATH_MAX];
 char inp[INP_MAX];
+
+//set error messages wherever required
+//use strcmp in executeInBuiltCommand
+
 
 char* directorySet(char* cwd,char* swd){
 
@@ -42,11 +48,69 @@ void printSystemName(){
 		printf("<%s@%s:%s>",uName,sName,directory);
 	}
 
-} 
+}
+
+int setlsFlag(char* argsForCommand){
+	char arggs[10][10];
+	int k=0,j=0;
+	for (int i=0;i<strlen(argsForCommand);i++){
+		if (argsForCommand[i]!=' '){
+			arggs[k][j]=argsForCommand[i],j++;
+		} else {
+			arggs[k][j]='\0',k++,j=0;
+		}
+	}
+	arggs[k][j]='\0';
+	k++;
+	int ret=-1;
+	char lsValues[][10]={"","-l","-a","-al","-la"};
+	for (int i=0;i<k;i++){
+		for (int j=0;j<5;j++){
+			// printf("(%d)\n",strcmp(arggs[i],lsValues[j]));
+			if (!strcmp(arggs[i],lsValues[j]))
+				ret=j;
+		}
+	}
+	return ret;
+}
+
+void executeCommand(char* curCommand,char* argsForCommand){
+	if (strcmp(curCommand,"ls\n") || strcmp(curCommand,"ls")){
+		int lsFlag=setlsFlag(argsForCommand);
+		char dirname[100];
+		DIR*p;
+		struct dirent *d;
+		if (lsFlag==-1)
+			strcpy(dirname,argsForCommand);
+		else
+			strcpy(dirname,".");
+
+		p=opendir(dirname);
+		if(p==NULL)
+		  {
+		  perror("Cannot find directory");
+		  }
+		while(d=readdir(p)){
+			if (lsFlag==0){
+				if (d->d_name[0]!='.')
+				  printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
+			}
+			else if (lsFlag==1){
+					//implement ls -l
+			} else if (lsFlag==2){
+				printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
+			} else if (lsFlag>2) {
+				// implemnet ls -la
+			} else if (lsFlag < 0) {
+				if (d->d_name[0]!='.')
+					printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
+			}
+		}
+  	}
+}
 
 void executeInBuiltCommand(){
 
-	// printf("%s",inp);
 	char curCommand[INP_MAX];
 	for (int i=0,k=0;i<=strlen(inp);i++){
 		if (inp[i]!=';' && i<strlen(inp) && inp[i]!='\0')
@@ -83,9 +147,6 @@ void executeInBuiltCommand(){
 
 			} else if (curCommand[0+inOffset]=='e' && curCommand[1+inOffset]=='c' && curCommand[2+inOffset]=='h' && curCommand[3+inOffset]=='o' && (curCommand[4+inOffset]==' ' || curCommand[4+inOffset]=='\0')){
 
-				// for (int j=0;j<strlen(argsForCommand);j++){
-					// if (argsForCommand[j])
-				// }
 				if (argsForCommand[0]=='\"'){
 
 					for (int j=1;j<strlen(argsForCommand);j++){
@@ -120,6 +181,7 @@ void executeInBuiltCommand(){
 
 				}
 			} else {
+				executeCommand(curCommand,argsForCommand);
 			}
 			k=0;
 		}
