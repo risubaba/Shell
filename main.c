13d436721c285efdeaed42dfbcf270294e8c5bc1
risubaba <risubhjain1010@gmail.com>
2019-08-20 10:48:29 +0530

    completed ls

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 #include <limits.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 #define INP_MAX 100
 
@@ -65,7 +70,6 @@ int setlsFlag(char* argsForCommand){
 	char lsValues[][10]={"","-l","-a","-al","-la"};
 	for (int i=0;i<k;i++){
 		for (int j=0;j<5;j++){
-			// printf("(%d)\n",strcmp(arggs[i],lsValues[j]));
 			if (!strcmp(arggs[i],lsValues[j]))
 				ret=j;
 		}
@@ -73,8 +77,39 @@ int setlsFlag(char* argsForCommand){
 	return ret;
 }
 
+void lsPrint(char* name){
+	struct passwd *pw;
+	struct group *gp;
+	struct stat sb;
+
+	stat(name,&sb);
+
+	printf( (S_ISDIR(sb.st_mode))  ? "d" : "-");
+    printf( (sb.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (sb.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (sb.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (sb.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (sb.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (sb.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (sb.st_mode & S_IROTH) ? "r" : "-");
+    printf( (sb.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (sb.st_mode & S_IXOTH) ? "x" : "-");
+	printf(" ");
+    printf("%ld ",sb.st_nlink);
+    pw=getpwuid(sb.st_uid);
+    printf("%s ",pw->pw_name);
+    gp=getgrgid(sb.st_gid);
+    printf("%s ",gp->gr_name);
+    printf("%5ld ",sb.st_size);
+    char* c=ctime(&sb.st_mtime);
+    for(int i=4;i<=15;i++)
+      printf("%c",c[i]);
+    printf(" ");
+    printf("%s\n",name);
+}
+
 void executeCommand(char* curCommand,char* argsForCommand){
-	if (strcmp(curCommand,"ls\n") || strcmp(curCommand,"ls")){
+	if (!strcmp(curCommand,"ls ") || !strcmp(curCommand,"ls")){
 		int lsFlag=setlsFlag(argsForCommand);
 		char dirname[100];
 		DIR*p;
@@ -83,29 +118,33 @@ void executeCommand(char* curCommand,char* argsForCommand){
 			strcpy(dirname,argsForCommand);
 		else
 			strcpy(dirname,".");
-
 		p=opendir(dirname);
-		if(p==NULL)
-		  {
+		if(p==NULL){
 		  perror("Cannot find directory");
-		  }
-		while(d=readdir(p)){
-			if (lsFlag==0){
-				if (d->d_name[0]!='.')
-				  printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
-			}
-			else if (lsFlag==1){
-					//implement ls -l
-			} else if (lsFlag==2){
-				printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
-			} else if (lsFlag>2) {
-				// implemnet ls -la
-			} else if (lsFlag < 0) {
-				if (d->d_name[0]!='.')
+		} else {
+			
+			while(d=readdir(p)){
+				if (lsFlag==0){
+					if (d->d_name[0]!='.')
 					printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
+				}
+				else if (lsFlag==1){
+					if (d->d_name[0]!='.')
+						lsPrint(d->d_name);
+				} else if (lsFlag==2){
+					printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
+				} else if (lsFlag>2) {
+					lsPrint(d->d_name);
+				} else if (lsFlag < 0) {
+					if (d->d_name[0]!='.')
+						printf("%s %d\n",d->d_name,(d->d_type==DT_DIR));
+				}
+
 			}
 		}
-  	}
+  	} else {
+		  printf("reeeeeee\n");	
+	  }
 }
 
 void executeInBuiltCommand(){
@@ -122,6 +161,8 @@ void executeInBuiltCommand(){
 			int argsOffset=inOffset;
 			while (curCommand[argsOffset]!=' ' && curCommand[argsOffset]!='\0') argsOffset++;
 			while (curCommand[argsOffset]==' ') argsOffset++;
+			if (argsOffset<strlen(curCommand))
+				curCommand[argsOffset-1]='\0';
 			char argsForCommand[INP_MAX];
 
 			for (int j=argsOffset,l=0;j<=k;j++,l++){
